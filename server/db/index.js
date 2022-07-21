602af4e52680564b00a45d1a2103b37db8c99815
DelroyGayle <delroygayle01@gmail.com>
2022-07-21 20:47:06 +0100

    remove URL1

--- server/db/index.js
+++ server/db/index.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-const connectionString = process.env.DATABASE_URL1;
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
 	connectionString,
