151037ad6044e98fe2ad62d269659bcca1790b22
Berkeli Halmyradov <berkelih@gmail.com>
2022-07-27 23:28:51 +0100

    add env variables to tests

--- server/__tests__/server.test.js
+++ server/__tests__/server.test.js
@@ -4,6 +4,25 @@ import createServer from "../../server/utils/createServer";
 
 const app = createServer(path.join(__dirname, "static"));
 
+const OLD_ENV = process.env;
+
+beforeEach(() => {
+	jest.resetModules();
+	process.env = {
+		M2M_AUTH0_CLIENT_ID: "test",
+		M2M_AUTH0_CLIENT_SECRET: "test",
+		AUTH0_DOMAIN: "ldn8-final-project.eu.auth0.com",
+		AUTH0_AUDIENCE: "breteau-api",
+		AUTH0_CLIENT_ID: "test",
+		AUTH0_CONNECTION: "Username-Password-Authentication",
+		AUTH0_CONNECTION_ID: "test",
+	};
+});
+
+afterAll(() => {
+	process.env = OLD_ENV;
+});
+
 describe("Test if server is running", () => {
 	it("should return a 200 status code", (done) => {
 		request(app).get("/api").expect(200, done);
